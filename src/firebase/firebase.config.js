768547c36a15eead8468601d2db4a7d32c3ecff8
AmitHasan0 <dev.amithasan@gmailcom>
2025-10-21 17:48:40 +0600

    added firebase im my project

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDJldC8Q4J2rgI9UktamQnYduoo_lb5T0",
+  authDomain: "dragon-news-today-a599b.firebaseapp.com",
+  projectId: "dragon-news-today-a599b",
+  storageBucket: "dragon-news-today-a599b.firebasestorage.app",
+  messagingSenderId: "792788273389",
+  appId: "1:792788273389:web:f984c036d5e53c0171e4e4",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/pages/Register.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Register = () => {
+  return (
+    <div>
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <h2 className="font-semibold text-2xl text-center pt-5">
+            Register your account
+          </h2>
+          <div className="card-body">
+            <fieldset className="fieldset">
+              {/* name */}
+              <label className="label">Name</label>
+              <input type="text" className="input" placeholder="Name" />
+              {/* photo URL */}
+              <label className="label">Photo URL</label>
+              <input type="text" className="input" placeholder="Photo URL" />
+              {/* email */}
+              <label className="label">Email</label>
+              <input type="email" className="input" placeholder="Email" />
+              {/* password */}
+              <label className="label">Password</label>
+              <input type="password" className="input" placeholder="Password" />
+
+              <button className="btn btn-neutral mt-4">Register</button>
+              <p className="font-semibold text-center p-4">
+                Already have an account ?{" "}
+                <Link to="/auth/login" className="text-secondary">
+                  Login
+                </Link>
+              </p>
+            </fieldset>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
